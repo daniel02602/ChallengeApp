62b9e8871ceabfe93b841cdb52a5714a8466e906
Daniel Juruś <djurus12@gmail.com>
2024-01-15 21:18:18 +0100

    Moduł4 - zadanie domowe - popawka kolejna

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,7 +1,7 @@
 ﻿var name = "Daniel";
 var age = 35;
-bool sex = true; // true = mężczyzna , false = kobieta
-if (sex == false && age < 30)
+bool isMen = true;
+if (isMen == false && age < 30)
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
@@ -9,11 +9,7 @@ else if (name == "Ewa" && age == 30)
 {
     Console.WriteLine("Ewa, lat 30");
 }
-else if (sex == true && age < 18)
+else if (isMen == true && age < 18)
 {
     Console.WriteLine("Niepełnoletni mężczyzna");
-}
-else
-{
-
 }
