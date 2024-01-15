da1a3f3b6fedc64cf1e2f3a04ac1e28c3408cc12
Daniel Juruś <djurus12@gmail.com>
2024-01-15 21:02:07 +0100

    Moduł4 - zadanie domowe

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, Daniel! Jestem");
+﻿var imie = "Daniel";
+var wiek = 35;
+bool plec = true; // true = mężczyzna , false = kobieta
+if (plec == false && wiek > 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (imie == "Ewa" && wiek == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (plec == true && wiek < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+
+}
