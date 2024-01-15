153a15564eac478f5e62e2878fa51b9af968aa18
Daniel Juruś <djurus12@gmail.com>
2024-01-15 21:11:56 +0100

    Moduł4 - zadanie domowe - poprawka

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,15 +1,15 @@
-﻿var imie = "Daniel";
-var wiek = 35;
-bool plec = true; // true = mężczyzna , false = kobieta
-if (plec == false && wiek > 30)
+﻿var name = "Daniel";
+var age = 35;
+bool sex = true; // true = mężczyzna , false = kobieta
+if (sex == false && age < 30)
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
-else if (imie == "Ewa" && wiek == 30)
+else if (name == "Ewa" && age == 30)
 {
     Console.WriteLine("Ewa, lat 30");
 }
-else if (plec == true && wiek < 18)
+else if (sex == true && age < 18)
 {
     Console.WriteLine("Niepełnoletni mężczyzna");
 }
